c01a8b4dd84abdbd20fd381b9a816467fb20334b
BreninSul <42633769+BreninSul@users.noreply.github.com>
2025-01-30 23:55:10 +0100

    Add new technical headers for request/response logging configurations
    New headers for max body size, file caching thresholds, and masking settings were added to enhance logging capabilities. Updated the TECHNICAL_HEADERS list to include these additions for better configurability. Also, incremented the version to 1.2.2 in the build file.

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,7 +37,7 @@ val kotlinVersion = "2.0.0"
 val javaVersion = JavaVersion.VERSION_17
 
 group = "io.github.breninsul"
-version = "1.2.0"
+version = "1.2.2"
 
 java {
     sourceCompatibility = javaVersion
