5e1c19c01cbac1f6421fa62ef5093c2ec0da5054
BreninSul <42633769+BreninSul@users.noreply.github.com>
2025-01-31 11:01:57 +0100

    Update version and rename `maskFormUrlencodedBodyKeys`
    Bumped project version to 1.3.0 in `build.gradle.kts`. Renamed `maskFormUrlencodedBodyKeys` to `maskFormBodyKeys` in `HttpMaskSettings` for better clarity, marking the change with `@since 1.3.0`.

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,7 +37,7 @@ val kotlinVersion = "2.0.0"
 val javaVersion = JavaVersion.VERSION_17
 
 group = "io.github.breninsul"
-version = "1.2.3"
+version = "1.3.0"
 
 java {
     sourceCompatibility = javaVersion
--- src/main/kotlin/io/github/breninsul/logging/HttpMaskSettings.kt
+++ src/main/kotlin/io/github/breninsul/logging/HttpMaskSettings.kt
@@ -7,11 +7,11 @@ package io.github.breninsul.logging
  * @param maskQueryParameters A list of query parameter names whose values should be masked. Default value is `listOf("Authorization", "authorization", "token", "secret", "password
  * ", "code")`. Only for request
  * @param maskJsonBodyKeys A list of JSON body keys whose values should be masked. Default value is `listOf("password", "pass", "code", "token", "secret")`.
- * @param maskFormUrlencodedBodyKeys A list of form-urlencoded body keys whose values should be masked. Default value is `listOf("password", "pass", "code", "token", "secret")`.
+ * @param maskFormBodyKeys A list of form-urlencoded body keys whose values should be masked. Default value is `listOf("password", "pass", "code", "token", "secret")`.  Renamed from `maskFormUrlencodedBodyKeys` @since `1.3.0`
  */
 open class HttpMaskSettings(
     var maskHeaders: List<String> = listOf("Authorization"),
     var maskQueryParameters: List<String> = listOf("Authorization","authorization","token", "secret","password","code"),
     var maskJsonBodyKeys: List<String> = listOf("password", "pass", "code", "token", "secret"),
-    var maskFormUrlencodedBodyKeys: List<String> = listOf("password", "pass", "code", "token", "secret"),
+    var maskFormBodyKeys: List<String> = listOf("password", "pass", "code", "token", "secret"),
 ) 
